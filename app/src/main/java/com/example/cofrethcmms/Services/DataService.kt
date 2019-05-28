f67d78b094a4ccc29515652a6401b019f05d23ad
Chang <changszehong93@gmail.com>
2019-05-28 14:11:08 +0800

    RecyclerView Adapter in progress

--- app/src/main/java/com/example/cofrethcmms/Services/DataService.kt
+++ app/src/main/java/com/example/cofrethcmms/Services/DataService.kt
@@ -1,9 +1,6 @@
 package com.example.cofrethcmms.Services
-
 import com.example.cofrethcmms.Model.WorkOrder
-
 object DataService {
-
     val categories = listOf(
         WorkOrder("Chiller","chiller"),
         WorkOrder("ahu","ahu"),
