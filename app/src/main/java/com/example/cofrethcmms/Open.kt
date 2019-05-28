95ab0a4d15d7ce1f1140377fba20bfde75108559
Chang <changszehong93@gmail.com>
2019-05-28 17:31:58 +0800

    Troubleshooting RecyclerView not showing in open tab

--- app/src/main/java/com/example/cofrethcmms/Open.kt
+++ app/src/main/java/com/example/cofrethcmms/Open.kt
@@ -2,17 +2,34 @@ package com.example.cofrethcmms
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.cofrethcmms.Services.DataService
+import com.example.cofrethcmms.ui.main.RecycleViewAdapter
+import kotlinx.android.synthetic.main.fragment_open.*
+import com.example.cofrethcmms.MainActivity as MainActivity
 
 class Open: Fragment() {
+
+    lateinit var adapter: RecycleViewAdapter
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-
-
-
-
-        return inflater.inflate(com.example.cofrethcmms.R.layout.fragment_open, container, false)
+        return inflater.inflate(R.layout.fragment_open, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        //val rview_keys: RecyclerView = findViewById(R.id.rview_keys)
+        workOrderListView.layoutManager = LinearLayoutManager(context)
+    }
+
 }
--- app/src/main/java/com/example/cofrethcmms/ui/main/RecycleViewAdapter.kt
+++ app/src/main/java/com/example/cofrethcmms/ui/main/RecycleViewAdapter.kt
@@ -2,15 +2,42 @@ package com.example.cofrethcmms.ui.main
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import com.example.cofrethcmms.Model.WorkOrder
+import com.example.cofrethcmms.Open
 import com.example.cofrethcmms.R
 
 class RecycleViewAdapter(val context: Context, val WorkOrder: List<WorkOrder>) : RecyclerView.Adapter<RecycleViewAdapter.Holder>(){
 
-    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!){
-        val WorkOrderImage = itemView?.findViewById<ImageView>(R.id.WorkOrderName)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val v = LayoutInflater.from(context).inflate(R.layout.workorder_list_item, parent,false)
+        return Holder(v)
     }
 
+    override fun getItemCount(): Int {
+        return WorkOrder.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder?.bindCategory(WorkOrder[position], context)
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!){
+        val workOrderImage = itemView?.findViewById<ImageView>(R.id.workOrderImage)
+        val workOrderTitle = itemView?.findViewById<TextView>(R.id.workOrderTitle)
+
+        fun bindCategory(WorkOrder: WorkOrder, context: Context){
+            val resourceId = context.resources.getIdentifier(WorkOrder.image, "drawable", context.packageName)
+            workOrderImage?.setImageResource(resourceId)
+            workOrderTitle?.text = WorkOrder.title
+        }
+    }
+
+
+
 }
+
